be9ddc4556d11595b47c0d3e173fe7aa151d30e8
Kellenkern <kellenrocha201711@gmail.com>
2022-11-25 00:58:02 -0300

    branch_kellen

--- login/index.php
+++ login/index.php
@@ -3,32 +3,25 @@
   <meta charset="UTF-8" />
   <title>Zodak</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-  <link href="index.css" type="text/css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
+  <link href="../css/login.css" rel="stylesheet">
 </head>
 <body id="bodyclass">
-   <div class="sidenav">
+<div class="main">
       <div class="login-main-text">
          <h2>ZODAK</h2>
          <p>IFsul Campus Sapiranga</p>
       </div>
-   </div>
-   <div class="main">
-      <div class="col-md-6">
-         <div class="login-form">
+      <div class="col-md-4 login-form">
             <form method="post" action="/v1/login/auth.php">
                <div class="form-group">
-                  <label for="login">User Name</label>
-                  <input id="login" class="form-control" name="login" required="required" type="text" placeholder="ex. 20191INF1234"/>
+                  <input id="login" class="form-control" name="login" required="required" type="text" placeholder="Username"/>
                </div>
                <div class="form-group">
-                  <label>Password</label>
-                  <input id="senha" name="senha" class="form-control" required="required" type="password" placeholder="ex. 1234"/>
+                  <input id="senha" name="senha" class="form-control" required="required" type="password" placeholder="Senha"/>
                </div>
-               <button type="submit" class="btn btn-black" value="Logar">Login</button>
+               <button type="submit" class="btn" value="Logar">Login</button>
             </form>
-         </div>
       </div>
    </div> 
 
